62d824893b0752160f473b27d90d743e9e994945
Josefa Miller <josefamillerb@gmail.com>
2023-05-26 17:34:31 -0400

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
 	return (
-		<div className="container">
+		<div className="imgHome">
+			<div className="container">
+			<h1>To do list!</h1>
 			<ul>
-				<li>Hacer la cama</li>
-				<li>Leer un libro</li>
-				<li>Hacer yoga</li>
-				<li>Salir a dar un paseo</li>
+				<li><input type="text"
+				onChange={(e) => setInputValue(e.target.value)}
+				value={inputValue}
+				onKeyPress={(e) => {
+					if (e.key === "Enter") {
+						setTodos(todos.concat(inputValue));
+						setInputValue("");
+					}
+				 }}
+				placeholder="¿Qué necesitas hacer hoy?"></input></li>
+				{todos.map((item, index) => (
+					<li>{item}<i className="trash" class="fa-solid fa-trash fa-2x" onClick={() =>
+						setTodos(
+							todos.filter(
+								(t, currentIndex) =>
+								index != currentIndex
+							))}></i></li>
+				))
+				}
+				
 			</ul>
+			<div>{todos.length} tareas</div>
 		</div>
+		</div>
+		
 	);
 };
 
